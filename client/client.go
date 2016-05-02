587b2593091926f9836492dcfbddaba3ee9071ff
William Langford <wlangfor@gmail.com>
2016-05-02 14:21:13 -0400

    Use lowercase for import paths for sanity

--- client/client.go
+++ client/client.go
@@ -8,10 +8,6 @@ import (
 	"strconv"
 	"time"
 
-	"golang.org/x/oauth2"
-	"golang.org/x/oauth2/google"
-	//"code.google.com/p/goauth2/oauth"
-	//"code.google.com/p/goauth2/oauth/jwt"
 	bigquery "github.com/dailyburn/google-api-go-client-bigquery/bigquery/v2"
 )
 
